b9f88c88deeb5542f2b3d224b71b28c935da8af0
avirads <avirads@gmail.com>
2026-01-17 20:46:55 +0530

    Initial commit: Professional Terminal for Worldwide Network Explorer

---
+++ sw.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = 'ip-asn-lookup-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './sql-wasm.wasm',
+  './sql-wasm.js',
+  './libarchive/libarchive.js',
+  './libarchive/worker-bundle.js',
+  './libarchive/libarchive.wasm',
+  './ip-to-asn-20260116.7z',
+  './manifest.json',
+  './leaflet.js',
+  './leaflet.css',
+  './favicon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.endsWith('.7z') || url.pathname.endsWith('.db')) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((cached) => {
+          if (cached) return cached;
+          return fetch(event.request).then((response) => {
+            if (response.ok) {
+              cache.put(event.request, response.clone());
+            }
+            return response;
+          });
+        });
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      });
+    })
+  );
+});
